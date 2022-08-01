79150aeacd6afefe53c2fa2bd4377319a1c28126
David <itsdavid71@gmail.com>
2022-08-01 16:14:48 +0300

    up

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once 'parts/header.php';
 include_once 'parts/banner.php';
 include_once 'parts/catalog.php';
@@ -7,5 +6,4 @@ include_once 'parts/faq.php';
 include_once 'parts/questions-form.php';
 include_once 'parts/cart.php';
 include_once 'parts/footer.php';
-
-
+?>
